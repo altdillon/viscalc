41f2ac5ce7d9b33cd4570673a9d6a0390d60f7a7
Dillon <dmanhall@gmail.com>
2016-05-12 22:12:16 -0600

    added exsample graph from chartjs to make sure it's nice and responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,5 +17,30 @@ requirejs.config({
 
 // require(["module/name", ...], function(params){ ... });
 require(["jquery","mathjs","chartjs", "jquery.bootstrap"], function ($,math,chart) {
-    $('#myModalButton').show();
+
+    //set up the graph
+    var ctx=document.getElementById('appgraph'); //get the canvas context
+
+    //start up an exsample graph
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3]
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+  });
+
+    debugger;
 });
